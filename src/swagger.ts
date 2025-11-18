9a1e44bf725f3f654d4d1b99034249c5ba9eacea
Akshai-muraleedharan <akshaimuraleedharan4@gmail.com>
2025-11-18 14:54:19 +0530

    swagger file update

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,6 +1,8 @@
 
 import swaggerJSDoc from "swagger-jsdoc";
+import path from "path";
 
+const apiPaths = process.env.NODE_ENV === "production" ? [path.join(__dirname, "./Routes/**/*.js")] : ["./src/Routes/**/*.ts"]
 
 const options = {
     definition: {
@@ -10,9 +12,9 @@ const options = {
             version: "1.0.0",
             description: "A sample API with swagger documentation"
         },
-        servers: [{ url: "http://localhost:3000/api/v1" }]
+        servers: [{ url: process.env.NODE_ENV === "production" ? `${process.env.PRODUCTION_URL}/api/v1` : "http://localhost:3000/api/v1" }]
     },
-    apis: ["./index/*.ts", "./src/routes/*.ts", "./src/controllers/*.ts"]
+    apis: apiPaths
 }
 
 
