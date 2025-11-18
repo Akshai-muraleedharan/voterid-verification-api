903076688aea359a6682aadbdca50f0bea101721
Akshai-muraleedharan <akshaimuraleedharan4@gmail.com>
2025-11-18 15:05:19 +0530

    swagger file update

--- dist/swagger.js
+++ dist/swagger.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+const path_1 = __importDefault(require("path"));
+const apiPaths = process.env.NODE_ENV === "production" ? [path_1.default.join(__dirname, "./Routes/**/*.js")] : ["./src/Routes/**/*.ts"];
 const options = {
     definition: {
         openapi: "3.0.0",
@@ -12,9 +14,9 @@ const options = {
             version: "1.0.0",
             description: "A sample API with swagger documentation"
         },
-        servers: [{ url: "http://localhost:3000/api/v1" }]
+        servers: [{ url: process.env.NODE_ENV === "production" ? `${process.env.PRODUCTION_URL}/api/v1` : "http://localhost:3000/api/v1" }]
     },
-    apis: ["./index/*.ts", "./src/routes/*.ts", "./src/controllers/*.ts"]
+    apis: apiPaths
 };
 const swaggerSpec = (0, swagger_jsdoc_1.default)(options);
 exports.default = swaggerSpec;
